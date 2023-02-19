f92186c4bacc27c34819b2ce828811cfd64732ea
BrianDYKim <18k7102dy@naver.com>
2023-02-20 00:04:35 +0900

    delete 로직 작성 완료

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Prisma } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -8,5 +18,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  async createUser(@Body() payload: Prisma.UserCreateInput) {
+    return await this.appService.createUser(payload);
+  }
+
+  @Post('/many')
+  async createManyUser(@Body() body) {
+    return await this.appService.createManyUsers();
+  }
+
+  @Post('/post')
+  async createPost(@Body() payload: Prisma.PostUncheckedCreateInput) {
+    return await this.appService.createPost(payload);
+  }
+
+  @Get('/users/user')
+  async getOneUserWithPost(@Query('id', ParseIntPipe) userId: number) {
+    return await this.appService.getUserWithPost(userId);
+  }
+
+  @Delete('/users')
+  async deleteUser(@Query('id', ParseIntPipe) userId: number) {
+    return await this.appService.deleteUser(userId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,88 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { faker } from '@faker-js/faker';
+import { Post, Prisma, User, UserInfo } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(payload: Prisma.UserCreateInput) {
+    const newUser: User = await this.prisma.user.create({
+      data: {
+        name: payload.name,
+        email: faker.datatype.uuid(),
+        profile: payload.profile,
+        userInfo: {
+          create: {
+            height: '174',
+            weight: 74,
+            address: faker.address.city(),
+          },
+        },
+      },
+      include: {
+        userInfo: true,
+      },
+    });
+
+    return newUser;
+  }
+
+  async createManyUsers() {
+    const data = new Array(1000000).fill({}).map((_) => ({
+      name: faker.name.firstName(),
+      email: faker.datatype.uuid(),
+      profile: faker.lorem.sentences(),
+    }));
+
+    const newUserList = await this.prisma.user.createMany({
+      data,
+    });
+
+    return newUserList;
+  }
+
+  async createPost(payload: Prisma.PostUncheckedCreateInput) {
+    const newPost: Post = await this.prisma.post.create({
+      data: {
+        content: faker.lorem.paragraph(),
+        writerId: Math.round(Math.random() * 50000) + 30000,
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    return newPost;
+  }
+
+  async getUserWithPost(userId: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        userId,
+      },
+      include: {
+        posts: true,
+      },
+    });
+  }
+
+  async deleteUser(userId: number): Promise<User> {
+    const deletedUser: User = await this.prisma.user.delete({
+      where: {
+        userId,
+      },
+      include: {
+        posts: true,
+      },
+    });
+
+    return deletedUser;
   }
 }
