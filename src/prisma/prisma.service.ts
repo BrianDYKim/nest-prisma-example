0707f6525c4c6cd68adfa5f380b232180785f072
BrianDYKim <18k7102dy@naver.com>
2023-02-19 17:18:04 +0900

    prisma 미들웨어 및 설정 완료

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,8 +3,22 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [{ emit: 'stdout', level: 'query' }],
+      errorFormat: 'pretty',
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+
+    // Middleware
+    this.$use(async (params, next) => {
+      console.log(params);
+
+      return await next(params);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
